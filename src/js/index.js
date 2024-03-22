d99f905aaa754d67fc3a2ed444d272d7d4d5b963
Walter Rivero <127254776+walter10x@users.noreply.github.com>
2024-03-22 15:44:51 +0000

    modificando archivo para mostrar cambios en la web

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,16 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
-
+import Navbar from "/workspaces/walter10x-Landing-Page-con-React/src/js/navbar"
+import Jumbotron from "/workspaces/walter10x-Landing-Page-con-React/src/js/jumbotron.jsx"
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(
+    <>
+      <Navbar />
+      <Jumbotron />
+      <Home />
+    </>,
+    document.querySelector("#app")
+  );
+
+
