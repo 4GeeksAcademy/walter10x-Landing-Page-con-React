a4d26cbcea55407a0fb02551f1a298f038c9446c
Walter Rivero <127254776+walter10x@users.noreply.github.com>
2024-03-22 16:02:29 +0000

    mostrando imagenes en website

--- src/js/index.js
+++ src/js/index.js
@@ -9,12 +9,15 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 import Navbar from "/workspaces/walter10x-Landing-Page-con-React/src/js/navbar"
 import Jumbotron from "/workspaces/walter10x-Landing-Page-con-React/src/js/jumbotron.jsx"
+import Card from "/workspaces/walter10x-Landing-Page-con-React/src/js/card.jsx"
+
 //render your react application
 ReactDOM.render(
     <>
       <Navbar />
       <Jumbotron />
       <Home />
+      <Card />
     </>,
     document.querySelector("#app")
   );
