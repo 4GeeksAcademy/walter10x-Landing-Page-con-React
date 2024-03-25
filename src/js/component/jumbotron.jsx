5f7448dee74cb369c7555482a115d22707c49b72
Walter Rivero <127254776+walter10x@users.noreply.github.com>
2024-03-25 09:07:37 +0000

    agregando boton hamburguesa y modificaciones al jumbotron

--- src/js/component/jumbotron.jsx
+++ src/js/component/jumbotron.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Jumbotron = () => {
     return (
-        <div className="jumbotron bg-light border p-3" style={{ width: "83%", margin: "10px auto" }}>
-            <h1 className="display-4"><strong>A Warm Welcome!</strong></h1>
+        <div className="jumbotron bg-light border p-4 pb-4" style={{ width: "83%", margin: "10px auto", height:"400px" }}>
+            <h1 className="display-4 pt-5"><strong>A Warm Welcome!</strong></h1>
             <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
             <hr className="my-4" />
             <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -5,21 +5,25 @@ const Navbar = () => {
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
                 <a className="navbar-brand mr-auto" href="#">Start Bootstrap</a>
-               
-                <ul className="navbar-nav ml-auto">
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Home</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">About</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Service</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Contact</a>
-                    </li>
-                </ul>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">About</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Service</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     );
